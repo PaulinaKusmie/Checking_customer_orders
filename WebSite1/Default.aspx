38096dfceb0633ce73c6cc9b8d0a8dd439885905
PaulinaKusmie <p-kusmierz@wp.pl>
2020-09-13 16:15:43 +0200

    Dodaj pliki projektów.

---
+++ WebSite1/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="SqlDataSource1" DataTextField="CustomerID" DataValueField="CustomerID" Height="41px" Width="238px">
+        </asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:NORTHWNDConnectionString %>" SelectCommand="SELECT [CustomerID] FROM [Customers]"></asp:SqlDataSource>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Pokaż zamówienie" Width="178px" />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <asp:GridView ID="GridView2" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        PobierzDaneZamowienia(DropDownList1.SelectedItem.Text);
+    }
+
+    protected void PobierzDaneZamowienia(string idKlienta)
+    {
+        DataSet mojZbiorDanych = new DataSet();
+        CustomersTableAdapter klientDA = new CustomersTableAdapter();
+        OrdersTableAdapter zamowieniaDA = new OrdersTableAdapter();
+
+        klientDA.Fill(mojZbiorDanych.Customers, idKlienta);
+        zamowieniaDA.Fill(mojZbiorDanych.Orders, idKlienta);
+
+        GridView1.DataSource = mojZbiorDanych.Tables["Customers"];
+        GridView1.DataBind();
+
+        GridView2.DataSource = mojZbiorDanych.Tables["Orders"];
+        GridView2.DataBind();
+
+
+    }
+}
